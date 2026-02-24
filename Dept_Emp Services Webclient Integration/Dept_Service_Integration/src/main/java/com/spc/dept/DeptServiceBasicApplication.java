c5428cdd9938636be601b2e882e538fa21fb751b
shalaka <gujarshalaka@gmail.com>
2026-02-24 18:51:31 +0530

    commit - Webclient integration microservices code

---
+++ Dept_Emp Services Webclient Integration/Dept_Service_Integration/src/main/java/com/spc/dept/DeptServiceBasicApplication.java
@@ -0,0 +1,14 @@
+package com.spc.dept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DeptServiceBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeptServiceBasicApplication.class, args);
+	}
+
+}
---
+++ Dept_Emp Services Webclient Integration/Emp_Service_Integration/src/main/java/com/spc/emp/EmpServiceBasicApplication.java
@@ -0,0 +1,14 @@
+package com.spc.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EmpServiceBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpServiceBasicApplication.class, args);
+	}
+
+}
